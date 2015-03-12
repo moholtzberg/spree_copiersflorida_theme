0ca425bd8d7ee6036ffc6e7d7e622e3370da9672
Moshe Holtzberg <moholtzberg@gmail.com>
2015-03-12 13:21:51 -0400

    comment out deface override calling reviews partials

--- app/overrides/reviews.rb
+++ app/overrides/reviews.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(virtual_path: 'spree/products/show',
-                     name: 'converted_product_properties_767643482',
-                     replace_contents: '[data-hook="product_reviews"]',
-                     partial: 'spree/shared/reviews',
-                     disabled: false)
+# Deface::Override.new(virtual_path: 'spree/products/show',
+#                      name: 'converted_product_properties_767643482',
+#                      replace_contents: '[data-hook="product_reviews"]',
+#                      partial: 'spree/shared/reviews',
+#                      disabled: false)
