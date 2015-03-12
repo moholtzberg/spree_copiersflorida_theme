6455c5df87b93dedd8b61af68d2b7ef55ce0297d
Moshe Holtzberg <moholtzberg@gmail.com>
2015-03-11 23:45:41 -0400

    remove inquires refactored bases new messages gem

--- app/models/spree/user_decorator.rb
+++
@@ -1,5 +0,0 @@
-Spree.user_class.class_eval do
-  # has_many :form_submissions, ->(object) {where("email = ?", object.email)}, :class_name => Spree::FormSubmission
-  has_many :inquiries, :foreign_key => :email, :primary_key => :email, :class_name => Spree::Inquiry
-    
-end
--- app/overrides/add_inquiries_to_admin_configuration_sidebar.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
-                     :name => "form_submissions_converted_admin_configurations_menu",
-                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-                     :text => "<%= configurations_sidebar_menu_item Spree.t('spree_inquiries.inquiries'), admin_inquiries_path %>",
-                     :disabled => false)
