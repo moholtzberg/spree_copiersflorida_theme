f589cb097b046d5c54c7ccefc1ac521e0e24439a
Moshe Holtzberg <moholtzberg@gmail.com>
2015-03-13 15:29:06 -0400

    removed_email_to_friend link

--- app/overrides/reviews.rb
+++ app/overrides/reviews.rb
@@ -1,5 +1,6 @@
 # Deface::Override.new(virtual_path: 'spree/products/show',
-#                      name: 'converted_product_properties_767643482',
-#                      replace_contents: '[data-hook="product_reviews"]',
-#                      partial: 'spree/shared/reviews',
-#                      disabled: false)
+#   name: 'converted_product_properties_767643482',
+#   replace_contents: '[data-hook="product_reviews"]',
+#   partial: 'spree/shared/reviews',
+#   disabled: false
+# )
