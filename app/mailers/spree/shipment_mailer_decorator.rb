5dc06ab44de29c4c95ce06818223964f272843d3
Moshe Holtzberg <moholtzberg@gmail.com>
2015-06-29 16:12:43 -0400

    order and shipment mailers updated to malign from sendgrid

--- app/mailers/spree/shipment_mailer_decorator.rb
+++ app/mailers/spree/shipment_mailer_decorator.rb
@@ -1,12 +1,12 @@
-Spree::OrderMailer.class_eval do
-  
-  def confirm_email(order, resend = false)
-    @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+Spree::ShipmentMailer.class_eval do
+
+  def shipped_email(shipment, resend = false)
+    @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-    subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
+    subject += "#{Spree::Store.current.name} #{Spree.t('shipment_mailer.shipped_email.subject')} ##{@shipment.order.number}"
     puts "=================="
     puts "=================="
-    puts "=====#{@order.email}====="
+    puts "=====#{@shipment.order.email}====="
     puts "=================="
     puts "=================="
     mg_client = Mailgun::Client.new(SECRET["MAILGUN_API_KEY"])
@@ -14,7 +14,7 @@ Spree::OrderMailer.class_eval do
       :to => @order.email,
       :from => 'orders@copiersfliorida.com',
       :subject => subject,
-      :html => render_to_string("spree/mailers/confirm_email", :layout => "spree/layouts/base_email").to_str
+      :html => render_to_string("spree/mailers/shipment_email", :layout => "spree/layouts/base_email").to_str
     }
     puts mg_client.send_message("mg.copiersflorida.com", message_params)
   end
