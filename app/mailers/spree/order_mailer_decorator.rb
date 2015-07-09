d15b0fbaab2f48a13caed431d3f98e690e85a893
Moshe Holtzberg <moholtzberg@gmail.com>
2015-07-09 14:36:41 -0400

    cancel_email now using mailgun

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -5,7 +5,26 @@ Spree::OrderMailer.class_eval do
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
     subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
     puts "=================="
+    puts "=====Confirm Email====="
+    puts "=====#{@order.email}====="
     puts "=================="
+    puts "=================="
+    mg_client = Mailgun::Client.new(SECRET["MAILGUN_API_KEY"])
+    message_params = {
+      :to => @order.email,
+      :from => 'orders@copiersfliorida.com',
+      :subject => subject,
+      :html => render_to_string("spree/mailers/confirm_email", :layout => "spree/layouts/base_email").to_str
+    }
+    puts mg_client.send_message("mg.copiersflorida.com", message_params)
+  end
+  
+  def cancel_email(order, resend = false)
+    @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+    subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
+    subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.cancel_email.subject')} ##{@order.number}"
+    puts "=================="
+    puts "=====Cancel Email====="
     puts "=====#{@order.email}====="
     puts "=================="
     puts "=================="
